f0fc28164f157eaf155a64c8d8976da4844b874c
Patel Divy <divypatel0403@gmail.com>
2024-06-20 00:25:40 +0530

    Paraphraser Removed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {Provider} from 'react-redux'
-import store from './services/store'; 
+import store from '../Backend/services/store.js'; 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
