a0047b24e68f4707db45eb5ac8648ae52e943213
Harsh Choksi <hhchoksi.976@gmail.com>
2024-06-20 21:08:26 +0530

    some changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import nodePolyfills from 'rollup-plugin-node-polyfills';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    nodePolyfills(),
+    rollupNodePolyFill(),
+    react()
+  ],
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      assert: 'assert',
+      http: 'stream-http',
+      https: 'https-browserify',
+      os: 'os-browserify',
+      url: 'url'
+    }
+  },
   build: {
     target: 'esnext',
   },
